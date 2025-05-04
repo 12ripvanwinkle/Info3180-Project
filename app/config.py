04cbb17766466816b81860d45014c09bf64b9a0f
JemmiJ <jemoiae@gmail.com>
2025-05-04 14:35:50 -0500

    render management part 14

--- app/config.py
+++ app/config.py
@@ -1,17 +1,18 @@
 import os
 from dotenv import load_dotenv
-from app import app
 
 load_dotenv()  # Load environment variables from .env if it exists.
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = os.path.join(app.root_path, 'static', 'uploads')
+    UPLOAD_FOLDER = os.path.join(basedir, 'static', 'uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
     # Ensure DATABASE_URL is set in .env and use PostgreSQL URI format
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # To suppress the warning, and as it is deprecated
+
