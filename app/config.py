30b5388ae2aa642064825cbcdd84eca77f90f0a6
JemmiJ <jemoiae@gmail.com>
2025-05-05 11:35:31 -0500

    testing

--- app/config.py
+++ app/config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env if it exists.
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
+STATIC_FOLDER = os.path.join(BASEDIR, 'static')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     DEBUG = False
 
     # Point at app/static, not project-root/static
-    STATIC_FOLDER = os.path.join(BASEDIR, 'static')
     UPLOAD_FOLDER = os.path.join(STATIC_FOLDER, 'uploads')
 
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
